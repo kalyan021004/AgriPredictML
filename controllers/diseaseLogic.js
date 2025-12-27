a4185303e3e9f33f43f9db0253561027c8ac7670
kalyan021004 <kalyan021004@gmail.com>
2025-12-27 20:36:48 +0530

    removing unnecceasry files

--- controllers/diseaseLogic.js
+++
@@ -1,7 +0,0 @@
-export async function detectDisease(req, res) {
-  res.json({
-    disease: "Leaf Blight",
-    confidence: 0.76,
-    treatment: "Use fungicide and ensure proper drainage"
-  });
-}
--- controllers/marketLogic.js
+++
@@ -1,7 +0,0 @@
-export async function predictPrice(req, res) {
-  res.json({
-    crop: req.body.crop,
-    price_per_quintal: 2450,
-    trend: "Stable"
-  });
-}
